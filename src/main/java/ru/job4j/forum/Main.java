6ccdb213e8341a59722af7c7263b86d6cc83867a
SlartiBartFast-art <JekHamond@tutanota.com>
2022-01-25 16:43:55 +0500

    0. Spring Liquibase [#302330]02 Уровень : 3. МидлКатегория : 3.4. SpringТопик : 3.4.7. Deploy

--- src/main/java/ru/job4j/forum/Main.java
+++ src/main/java/ru/job4j/forum/Main.java
@@ -1,12 +1,27 @@
 package ru.job4j.forum;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
 /**
  * Main class SpringBoot Start
+ * <p>
+ * Подключаем к проекту Liquibase -
+ * Liquibase - независимая от базы данных библиотека для отслеживания,
+ * управления и применения изменений схемы базы данных.
+ * + зависимости в pom.xml
+ * 0. Spring Liquibase [#302330]02
+ * Уровень : 3. МидлКатегория : 3.4. SpringТопик : 3.4.7. Deploy
+ * Liquibase будет запускаться каждый раз при старте приложения. *
+ * Этого эффекта мы добьемся через Spring.
+ * <p>
  * Многие разработчики Spring Boot всегда аннотируют свой основной класс с помощью @Configuration,
  * -@EnableAutoConfigurationи @ComponentScan. Поскольку эти аннотации так часто используются вместе
  * (особенно если вы следуете приведенным выше рекомендациям), Spring Boot предоставляет
@@ -20,6 +35,22 @@ public class Main extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Main.class);
+    }
+
+    /**
+     * Подключаем к проекту Liquibase -
+     * Liquibase - независимая от базы данных библиотека для отслеживания,
+     * управления и применения изменений схемы базы данных.
+     *
+     * @param ds
+     * @return
+     */
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 
     public static void main(String[] args) {
---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,10 @@
+insert into posts (description, name, user_id)
+values ('Продам велосипед, новый, горный, 25 скоростей Schimano',
+        'Продам велосипед', 1);
+insert into posts (description, name, user_id)
+values ('Куплю или приму в дар мотоцикл, б/у, находу',
+        'Куплю мотоцикл', 2);
+insert into posts (description, name, user_id)
+values ('Требования Java, JS, HTML, PostgreSQL, Git, Linux',
+        'Вакансия на стажера', 3);
+
